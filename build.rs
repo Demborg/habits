116acced3012d218675a71de61a3e8e878967adc
Axel Demborg <axel.demborg@gmail.com>
2023-05-28 21:14:24 +0200

    more debug?

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 use std::process::Command;
 
 fn main() {
-    println!("rm");
+    println!("rm crud");
     Command::new("rm")
         .args(&["-rf", "frontend/dist"])
         .status()
         .unwrap();
 
+    println!("build");
     Command::new("trunk")
         .args(&["build", "frontend/index.html"])
         .status()
--- frontend/dist/frontend-50e6985763e0697a.js
+++ frontend/dist/frontend-50e6985763e0697a.js
@@ -655,7 +655,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('frontend-1fca04a98b7bee79_bg.wasm', import.meta.url);
+        input = new URL('frontend-50e6985763e0697a_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
