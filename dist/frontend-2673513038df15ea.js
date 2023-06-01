4d2f093741e2cfd04a9ac69a257ee345c1349d22
Axel Demborg <axel.demborg@gmail.com>
2023-06-01 21:04:24 +0200

    Frontend reacts when you click a button, no communication with BE though

--- dist/frontend-2673513038df15ea.js
+++ dist/frontend-2673513038df15ea.js
@@ -719,12 +719,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3060 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 195, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper3335 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 239, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4697 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 329, __wbg_adapter_21);
+    imports.wbg.__wbindgen_closure_wrapper5134 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 383, __wbg_adapter_21);
         return addHeapObject(ret);
     };
 
@@ -766,7 +766,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('frontend-a04da2867dde95b2_bg.wasm', import.meta.url);
+        input = new URL('frontend-2673513038df15ea_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,22 @@
-use yew::prelude::*;
 use gloo_net::http::Request;
+use yew::prelude::*;
 
-use shared::{HabitWithCompletions};
+use shared::HabitWithCompletions;
+
+#[derive(Properties, PartialEq)]
+struct HabitProps {
+    habit: HabitWithCompletions,
+}
+#[function_component(Habit)]
+fn habit_list(HabitProps { habit }: &HabitProps) -> Html {
+    let clicks = use_state(|| 0);
+    let other_clicks = clicks.clone();
+    html! {
+        <div onclick={move |_| clicks.set(*clicks + 1)}>
+            <h3>{format!("{} {}/{} times {}", habit.habit.name, habit.completed + *other_clicks, habit.habit.reps, habit.habit.cadance)}</h3>
+        </div>
+    }
+}
 
 #[derive(Properties, PartialEq)]
 struct HabitListProps {
@@ -10,12 +25,12 @@ struct HabitListProps {
 
 #[function_component(HabitList)]
 fn habit_list(HabitListProps { habits }: &HabitListProps) -> Html {
-    habits.iter().map(|habit| {
-        html! {
-        <div>
-            <h3>{format!("{} {}/{} times {}", habit.habit.name, habit.completed, habit.habit.reps, habit.habit.cadance)}</h3>
-        </div>
-    }}).collect()
+    habits
+        .iter()
+        .map(|habit| {
+            html! {<Habit habit={habit.clone()} />}
+        })
+        .collect()
 }
 
 #[function_component(App)]
